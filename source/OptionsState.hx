8dd4ddb2165af56ad93a9eb47461d12d870103e5
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-07-30 10:00:29 +0700

    working option now

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -1,14 +1,16 @@
 package;
 
 import flixel.FlxG;
+import flixel.addons.ui.FlxButtonPlus; // i love the plus one
 import flixel.text.FlxText;
-import flixel.ui.FlxButton;
 
 class OptionsState extends MainState
 {
 	var text:FlxText;
+	var hintText:FlxText;
 	// button list
-	var harderMode:FlxButton;
+	var fullScreenButton:FlxButtonPlus;
+	var harderModeButton:FlxButtonPlus;
 
 	override public function create()
 	{
@@ -19,6 +21,24 @@ class OptionsState extends MainState
 		text.scrollFactor.set();
 		text.alignment = CENTER;
 		add(text);
+
+		hintText = new FlxText(10, FlxG.height - 22, 0, "PRESS ESC TO RETURN MAIN MENU", 12);
+		hintText.alignment = LEFT;
+		hintText.scrollFactor.set();
+		add(hintText);
+
+		addButton();
+	}
+
+	function addButton()
+	{
+		fullScreenButton = new FlxButtonPlus(100, 200, clickFullScreen, FlxG.fullscreen ? "FULLSCREEN" : "WINDOWED");
+		fullScreenButton.scrollFactor.set();
+		add(fullScreenButton);
+
+		harderModeButton = new FlxButtonPlus(100, 250, clickHarderMode, FlxG.save.data.harderMode ? "HARDER MODE: ON" : "HARDER MODE: OFF");
+		harderModeButton.scrollFactor.set();
+		add(harderModeButton);
 	}
 
 	override public function update(elapsed:Float)
@@ -29,5 +49,19 @@ class OptionsState extends MainState
 		{
 			FlxG.switchState(new MainMenuState());
 		}
+	}
+
+	function clickFullScreen()
+	{
+		FlxG.fullscreen = !FlxG.fullscreen;
+		fullScreenButton.text = FlxG.fullscreen ? "FULLSCREEN" : "WINDOWED";
+		FlxG.save.data.fullscreen = FlxG.fullscreen;
+	}
+
+	function clickHarderMode()
+	{
+		FlxG.save.data.harderMode = !FlxG.save.data.harderMode;
+		harderModeButton.text = FlxG.save.data.harderMode ? "HARDER MODE: ON" : "HARDER MODE: OFF";
+		FlxG.save.data.harderMode = FlxG.save.data.harderMode;
 	}
 }
